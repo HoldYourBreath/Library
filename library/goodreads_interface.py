6281420d3d0df373a695152835208c6a2d9cb041
Per Fryking <fryking@gmail.com>
2017-05-19 16:01:26 +0200

    Fetch more information from goodreads

--- library/goodreads_interface.py
+++ library/goodreads_interface.py
@@ -6,32 +6,81 @@ import xml.dom.minidom as minidom
 from app import app
 from config import config
 
-GOODREAD_URL = "https://www.goodreads.com/search/index.xml?key={KEY}&q={ISBN}"
+GOODREAD_ISBN_SEARCH_URL = "https://www.goodreads.com/search/index.xml?key={KEY}&q={ISBN}"
+GOODREAD_BOOK_URL = "https://www.goodreads.com/book/show/{BOOK_ID}?key={KEY}"
 
 @app.route('/api/books/goodread/<isbn>')
 def get_book(isbn):
-    reply = fetch_goodread(isbn)
-    dom = minidom.parseString(reply)
-    book = dom.getElementsByTagName('best_book')[0]
-    author = book.getElementsByTagName('author')[0].getElementsByTagName('name')[0].childNodes[0].nodeValue
-    title = book.getElementsByTagName('title')[0].childNodes[0].nodeValue
-    pub_date = [dom.getElementsByTagName('original_publication_year')[0].childNodes[0].nodeValue,
-            dom.getElementsByTagName('original_publication_month')[0].childNodes[0].nodeValue,
-            dom.getElementsByTagName('original_publication_day')[0].childNodes[0].nodeValue]
+    book_id = lookup_goodreads_id(isbn)
+    book = fetch_goodreads_book(book_id)
+    return get_json_response(book)
 
+
+def lookup_goodreads_id(isbn):
+    url = GOODREAD_ISBN_SEARCH_URL.replace("{KEY}", config.get('Goodreads', 'api_key'))
+    url = url.replace("{ISBN}", str(isbn))
+    response = urllib.urlopen(url)
+
+    raw = response.read()
+
+    dom = minidom.parseString(raw)
+    for el in dom.getElementsByTagName('best_book')[0].childNodes:
+        if el.nodeName == 'id':
+            return el.firstChild
+
+    raise LookupError("Can't find a book id for ISBN: {}".format(isbn))
+
+
+def fetch_goodreads_book(book_id):
+    url = GOODREAD_ISBN_SEARCH_URL.replace("{KEY}", config.get('Goodreads', 'api_key'))
+    url = url.replace("{BOOK_ID}", str(book_id))
+    response = urllib.urlopen(url)
+
+    raw = response.read()
+    return minidom.parseString(raw)
+
+    raise LookupError("Can't find a book id for book_id: {}".format(book_id))
+
+
+def get_json_response(book):
     response = {
-            'author': author,
-            'title': title,
-            'publication_date': ' '.join(pub_date)
+            'author': get_authors(book),
+            'title': get_title(book),
+            'publication_date': get_publication_date(book),
+            'num_pages': get_num_pages(book),
+            'description': get_description(book)
             }
+
     return json.dumps(response)
 
 
-def fetch_goodread(isbn):
-    url = GOODREAD_URL.replace("{KEY}", config.get('Goodreads', 'api_key')).replace("{ISBN}", str(isbn))
-    response = urllib.urlopen(url)
-    if response.getcode() != 200:
-        raise InfobankHttpError(response.getcode(), url)
+def get_authors(book):
+    authors = []
+    for node in book.getElementsByTagName('authors')[0].getElementsByTagName('name'):
+        authors.append(node.childNodes[0].nodeValue)
 
-    raw = response.read()
-    return raw
+    return authors
+
+
+def get_title(book):
+    return book.getElementsByTagName('title')[0].childNodes[0].nodeValue
+
+
+def get_publication_date(book):
+    date = [book.getElementsByTagName('publication_year')[0].childNodes[0].nodeValue,
+            book.getElementsByTagName('publication_month')[0].childNodes[0].nodeValue.zfill(2),
+            book.getElementsByTagName('publication_day')[0].childNodes[0].nodeValue.zfill(2)]
+    return ' '.join(date)
+
+
+def get_description(book):
+    description = book.getElementsByTagName('description')[0]
+
+    if description.hasChildNodes():
+        return description.childNodes[0].nodeValue
+
+    return ''
+
+
+def get_num_pages(book):
+    return int(book.getElementsByTagName('num_pages')[0].childNodes[0].nodeValue)
--- tests/test_server.py
+++ tests/test_server.py
@@ -2,18 +2,23 @@ import os
 import unittest
 import tempfile
 import json
+import ConfigParser
+from Queue import Queue, Empty
 
 # Local modules
 import library.server as server
 import library.database as database
 from library.config import config
 
-
-class UrllibStub():
-
+class Response():
     def __init__(self, code, data):
         self.code = code
         self.data = data
+
+class UrllibStub():
+
+    def __init__(self, responses):
+        self.responses = responses
 
     class Response():
 
@@ -28,13 +33,18 @@ class UrllibStub():
             return self.data
 
     def urlopen(self, url):
-        return self.Response(self.code, self.data)
+        try:
+            response = self.responses.get(False)
+        except Empty:
+            raise Empty("Unexpected URL fetch. Test stub queue empty")
+
+        return self.Response(response.code, response.data)
 
 
 class ServerTestCase(unittest.TestCase):
 
-    @classmethod
     def setUp(self):
+        global config
         # Set up a temporary database
         self.db_fd, server.app.config['DATABASE'] = tempfile.mkstemp()
         server.app.config['TESTING'] = True
@@ -43,13 +53,10 @@ class ServerTestCase(unittest.TestCase):
             database.init()
 
         # Set up a temporary config file
-        self.config_fd, temp_config = tempfile.mkstemp()
-        config.read(temp_config)
+        config = ConfigParser.ConfigParser()
 
-    @classmethod
     def tearDown(self):
         os.close(self.db_fd)
-        os.close(self.config_fd)
         os.unlink(server.app.config['DATABASE'])
 
 
@@ -113,66 +120,66 @@ class BookTestCase(ServerTestCase):
         rv = self.app.put('/api/books/{}'.format(book_id), data={'isbn': isbn})
         self.assertEqual(rv.status_code, 200)
 
-class GoodreadTestCase(ServerTestCase):
 
-    def test_get_book(self):
+class GoodreadsTestCase(ServerTestCase):
+
+    def setUp(self):
+        ServerTestCase.setUp(self)
         import library.goodreads_interface as gi
-        data = """\
-<?xml version="1.0" encoding="UTF-8"?>
-<GoodreadsResponse>
-  <Request>
-	<authentication>true</authentication>
-	  <key><![CDATA[PQkx0Upvp4Fyv9EtiB5Bg]]></key>
-	<method><![CDATA[search_index]]></method>
-  </Request>
-  <search>
-  <query><![CDATA[9780134052502]]></query>
-	<results-start>1</results-start>
-	<results-end>1</results-end>
-	<total-results>1</total-results>
-	<source>Goodreads</source>
-	<query-time-seconds>0.01</query-time-seconds>
-	<results>
-		<work>
-  <id type="integer">42757860</id>
-  <books_count type="integer">6</books_count>
-  <ratings_count type="integer">279</ratings_count>
-  <text_reviews_count type="integer">49</text_reviews_count>
-  <original_publication_year type="integer">2014</original_publication_year>
-  <original_publication_month type="integer">12</original_publication_month>
-  <original_publication_day type="integer">3</original_publication_day>
-  <average_rating>4.32</average_rating>
-  <best_book type="Book">
-	<id type="integer">23215733</id>
-	<title>The Software Craftsman: Professionalism, Pragmatism, Pride</title>
-	<author>
-	  <id type="integer">7127583</id>
-	  <name>Sandro Mancuso</name>
-	</author>
-	<image_url>https://images.gr-assets.com/books/1416778735m/23215733.jpg</image_url>
-	<small_image_url>https://images.gr-assets.com/books/1416778735s/23215733.jpg</small_image_url>
-  </best_book>
-</work>
-
-	</results>
-</search>
-
-</GoodreadsResponse>\
-"""
-        gi.urllib = UrllibStub(200, data)
+        self.response_queue = Queue()
+        gi.urllib = UrllibStub(self.response_queue)
 
         config.add_section('Goodreads')
         config.set('Goodreads', 'api_key', '123456789')
 
+        self.dir = os.path.dirname(__file__)
+
+
+    def test_get_book(self):
+        isbn_data = open(os.path.join(self.dir, "sample_response_isbn.xml")).read()
+        book_data = open(os.path.join(self.dir, "sample_response_book_id.xml")).read()
+        self.response_queue.put(Response(200, isbn_data))
+        self.response_queue.put(Response(200, book_data))
+
         expected_response = {
-                'author': 'Sandro Mancuso',
+                'author': ['Sandro Mancuso'],
                 'title': 'The Software Craftsman: Professionalism, Pragmatism, Pride',
-                'publication_date': '2014 12 3'
+                'publication_date': '2014 12 04',
+                'num_pages': 288,
+                'description': '<b>Be a Better Developer and Deliver Better Code</b> Despite advanced tools and methodologies, software projects continue to fail. Why? Too many organizations still view software development as just another production line. Too many developers feel that way, too and they behave accordingly. In <b> <i>The Software Craftsman: Professionalism, Pragmatism, Pride, </i> </b>Sandro Mancuso offers a better and more fulfilling path. If you want to develop software with pride and professionalism; love what you do and do it with excellence; and build a career with autonomy, mastery, and purpose, it starts with the recognition that you are a craftsman. Once you embrace this powerful mindset, you can achieve unprecedented levels of technical excellence and customer satisfaction. Mancuso helped found the world s largest organization of software craftsmen; now, he shares what he s learned through inspiring examples and pragmatic advice you can use in your company, your projects, and your career. You will learn<br />Why agile processes aren t enough and why craftsmanship is crucial to making them work How craftsmanship helps you build software right and helps clients in ways that go beyond code How and when to say No and how to provide creative alternatives when you do Why bad code happens to good developers and how to stop creating and justifying it How to make working with legacy code less painful and more productive How to be pragmatic not dogmatic about your practices and tools How to lead software craftsmen and attract them to your organization What to avoid when advertising positions, interviewing candidates, and hiring developers How developers and their managers can create a true culture of learning How to drive true technical change and overcome deep patterns of skepticism <b>Sandro Mancuso</b> has coded for startups, software houses, product companies, international consultancies, and investment banks. In October 2013, he cofounded Codurance, a consultancy based on Software Craftsmanship principles and values. His involvement with Software Craftsmanship began in 2010, when he founded the London Software Craftsmanship Community (LSCC), now the world s largest and most active Software Craftsmanship community, with more than two thousand craftsmen. For the past four years, he has inspired and helped developers to organize Software Craftsmanship communities throughout Europe, the United States, and the rest of the world."'
                 }
 
         rv = self.app.get('/api/books/goodread/1234')
-        self.assertEqual(rv.data, json.dumps(expected_response))
+        response = json.loads(rv.data)
+
         self.assertEqual(rv.status_code, 200)
+        self.verify_response(response, expected_response)
+
+    def test_multi_author(self):
+        isbn_data = open(os.path.join(self.dir, "sample_response_isbn_multi_author.xml")).read()
+        book_data = open(os.path.join(self.dir, "sample_response_book_id_multi_author.xml")).read()
+        self.response_queue.put(Response(200, isbn_data))
+        self.response_queue.put(Response(200, book_data))
+
+        expected_response = {
+                'author': ["Erik Dahlman", "Stefan Parkvall", "Johan Skold"],
+                'title': '4g: Lte/Lte-Advanced for Mobile Broadband',
+                'publication_date': '2013 12 20',
+                'num_pages': 544,
+                'description': ''
+                }
+
+        rv = self.app.get('/api/books/goodread/1234')
+        response = json.loads(rv.data)
+
+        self.assertEqual(rv.status_code, 200)
+        self.verify_response(response, expected_response)
+
+    def verify_response(self, response, expected_response):
+        self.assertEqual(response['author'], expected_response['author'])
+        self.assertEqual(response['title'], expected_response['title'])
+        self.assertEqual(response['publication_date'], expected_response['publication_date'])
+        self.assertEqual(response['description'], expected_response['description'])
 
 
 if __name__ == '__main__':
