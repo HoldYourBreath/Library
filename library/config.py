cecbd1cdaef03dbd852a923e904b28c722f9b536
Per Fryking <fryking@gmail.com>
2017-05-19 16:08:04 +0200

    Raise error in case no config file

--- library/config.py
+++ library/config.py
@@ -1,4 +1,11 @@
+import os
 import ConfigParser
+
+class ConfigError(IOError):
+    pass
+
+if not os.path.isfile('library.cfg'):
+    raise ConfigError("Config file not found!")
 
 config = ConfigParser.ConfigParser()
 config.read('library.cfg')
