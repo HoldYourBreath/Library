d1cd1962af59e3440d3faf517f9d7967452c411b
Per Fryking <fryking@gmail.com>
2017-05-22 13:00:33 +0200

    Split rediculous long line into several

--- tests/test_goodreads_api.py
+++ tests/test_goodreads_api.py
@@ -61,11 +61,64 @@ class GoodreadsTestCase(ServerTestCase):
         self.response_queue.put(Response(200, book_data))
 
         expected_response = {
-                'author': ['Sandro Mancuso'],
-                'title': 'The Software Craftsman: Professionalism, Pragmatism, Pride',
-                'publication_date': '2014 12 04',
-                'num_pages': 288,
-                'description': '<b>Be a Better Developer and Deliver Better Code</b> Despite advanced tools and methodologies, software projects continue to fail. Why? Too many organizations still view software development as just another production line. Too many developers feel that way, too and they behave accordingly. In <b> <i>The Software Craftsman: Professionalism, Pragmatism, Pride, </i> </b>Sandro Mancuso offers a better and more fulfilling path. If you want to develop software with pride and professionalism; love what you do and do it with excellence; and build a career with autonomy, mastery, and purpose, it starts with the recognition that you are a craftsman. Once you embrace this powerful mindset, you can achieve unprecedented levels of technical excellence and customer satisfaction. Mancuso helped found the world s largest organization of software craftsmen; now, he shares what he s learned through inspiring examples and pragmatic advice you can use in your company, your projects, and your career. You will learn<br />Why agile processes aren t enough and why craftsmanship is crucial to making them work How craftsmanship helps you build software right and helps clients in ways that go beyond code How and when to say No and how to provide creative alternatives when you do Why bad code happens to good developers and how to stop creating and justifying it How to make working with legacy code less painful and more productive How to be pragmatic not dogmatic about your practices and tools How to lead software craftsmen and attract them to your organization What to avoid when advertising positions, interviewing candidates, and hiring developers How developers and their managers can create a true culture of learning How to drive true technical change and overcome deep patterns of skepticism <b>Sandro Mancuso</b> has coded for startups, software houses, product companies, international consultancies, and investment banks. In October 2013, he cofounded Codurance, a consultancy based on Software Craftsmanship principles and values. His involvement with Software Craftsmanship began in 2010, when he founded the London Software Craftsmanship Community (LSCC), now the world s largest and most active Software Craftsmanship community, with more than two thousand craftsmen. For the past four years, he has inspired and helped developers to organize Software Craftsmanship communities throughout Europe, the United States, and the rest of the world."'
+            'author': ['Sandro Mancuso'],
+            'title': 'The Software Craftsman: Professionalism, '\
+                     'Pragmatism, Pride',
+            'publication_date': '2014 12 04',
+            'num_pages': 288,
+            'description':
+                '<b>Be a Better Developer and Deliv'\
+                'er Better Code</b> Despite advanced tools and '\
+                'methodologies, software projects continue to fail.'\
+                ' Why? Too many organizations still view software '\
+                'development as just another production line. Too '\
+                'many developers feel that way, too and they behave'\
+                ' accordingly. In <b> <i>The Software Craftsman: '\
+                'Professionalism, Pragmatism, Pride, </i> </b>Sandr'\
+                'o Mancuso offers a better and more fulfilling path.'\
+                ' If you want to develop software with pride and '\
+                'professionalism; love what you do and do it with '\
+                'excellence; and build a career with autonomy, '\
+                'mastery, and purpose, it starts with the recogniti'\
+                'on that you are a craftsman. Once you embrace this '\
+                'powerful mindset, you can achieve unprecedented '\
+                'levels of technical excellence and customer '\
+                'satisfaction. Mancuso helped found the world s '\
+                'largest organization of software craftsmen; now, '\
+                'he shares what he s learned through inspiring '\
+                'examples and pragmatic advice you can use in your '\
+                'company, your projects, and your career. You will '\
+                'learn<br />Why agile processes aren t enough and '\
+                'why craftsmanship is crucial to making them work '\
+                'How craftsmanship helps you build software right '\
+                'and helps clients in ways that go beyond code How'\
+                ' and when to say No and how to provide creative '\
+                'alternatives when you do Why bad code happens to '\
+                'good developers and how to stop creating and '\
+                'justifying it How to make working with legacy '\
+                'code less painful and more productive How to be '\
+                'pragmatic not dogmatic about your practices and '\
+                'tools How to lead software craftsmen and attract '\
+                'them to your organization What to avoid when '\
+                'advertising positions, interviewing candidates, '\
+                'and hiring developers How developers and their '\
+                'managers can create a true culture of learning '\
+                'How to drive true technical change and overcome '\
+                'deep patterns of skepticism <b>Sandro Mancuso</b> '\
+                'has coded for startups, software houses, product '\
+                'companies, international consultancies, and '\
+                'investment banks. In October 2013, he cofounded '\
+                'Codurance, a consultancy based on Software '\
+                'Craftsmanship principles and values. His '\
+                'involvement with Software Craftsmanship began in '\
+                '2010, when he founded the London Software '\
+                'Craftsmanship Community (LSCC), now the world s '\
+                'largest and most active Software Craftsmanship '\
+                'community, with more than two thousand craftsmen.'\
+                ' For the past four years, he has inspired and helped '\
+                'developers to organize Software Craftsmanship '\
+                'communities throughout Europe, the United States, '\
+                'and the rest of the world."'
                 }
 
         rv = self.app.get('/api/books/goodread/1234')
