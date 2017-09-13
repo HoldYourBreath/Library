1eab4b474fdaf231f6b241214502a7abfcc5d4c0
ejanmak <bacrguy@gmail.com>
2017-09-13 12:40:15 +0200

    Books - On click, add table on right side

--- library_js/src/components/books.js
+++ library_js/src/components/books.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Form } from 'react-bootstrap';
+import { render } from "react-dom";
 import { Tips } from "./Utils";
 import ReactTable from 'react-table'
 import 'react-table/react-table.css'
+import '../App.css';
 const request = require('superagent');
 
 
@@ -9,7 +12,19 @@ class Books extends Component {
   constructor() {
     super();
     this.state = {
-      books: []
+      books: [],
+      loadingBookData: false,
+      errorMsg: '',
+      infoMsg: '',
+      isbn: '',
+      tag: '',
+      title: '',
+      author: '',
+      description: '',
+      format: '',
+      pages: '',
+      publication_date: '',
+      thumbnail: ''
     };
   };
 
@@ -27,8 +42,42 @@ class Books extends Component {
   render() {
     console.log(this.state.books);
     return (
-      <div>
+    <div className="flex-container">
+      <div className="bookTable">
         <ReactTable
+          getTdProps={(state, rowInfo, column, instance) => {
+            return {
+              onClick: (e) => {
+                  if (rowInfo.original !== null) {
+                    let url = `${window.__appUrl}/api/books/goodreads/${rowInfo.original.isbn}`;
+                    this.setState({loadingBookData: true});
+                    request
+                      .get(url)
+                      .type('application/json')
+                      .on('error', (err) => {
+                        this.setState({
+                          errorMsg: `Unable to fetch book info for ${rowInfo.original.isbn}`,
+                          loadingBookData: false
+                        });
+                      })
+                      .end((err, res) => {
+                        if (err) {
+                          return;
+                        }
+                        this.setState({loadingBookData: false});
+                        this.setState({errorMsg: null});
+                        let state = Object.assign({}, res.body);
+                        state.pages = state.num_pages;
+                        delete state.num_pages;
+                        delete state.errorMsg;
+                        this.setState(state);
+                        }
+                      );
+                }
+               }
+              }
+            }
+          }
          data={this.state.books}
           columns={
           [
@@ -48,42 +97,40 @@ class Books extends Component {
               columns:
               [
                 {
-                  Header: "Authors",
-                  accessor: "authors"
-                }
-              ]
-            },
-            {
-              Header: "",
-              columns:
-              [
-                {
                   Header: "Published",
                   accessor: "publication_date",
-                  width: 90
-                }
-              ]
-            },
-            {
-              Header: '',
-              columns:
-              [
-                {
-                  Header: "Pages",
-                  accessor: "pages",
-                  width: 50
+                  width: 67
                 }
               ]
             }
           ]}
-          defaultPageSize={15}
+          defaultPageSize={20}
           className="-striped -highlight"
         />
         <br />
         <Tips />
       </div>
+      <div className="bookInfo">
+      <Form horizontal>
+        <div className="booksTitle">
+          {this.state.title}
+        </div>
+
+        <div className="booksAuthor">
+          {this.state.authors}    {this.state.format}    {this.state.pages}
+        </div>
+
+          <img id="" alt="" src={this.state.thumbnail} />
+
+        <div className="description">
+          {this.state.description}
+        </div>
+      </Form>
+      </div>
+    </div>
     );
   }
 }
-
+render(<Books />, document.getElementById("root"));
 export default Books;
+
